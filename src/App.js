ca4293e3a407f90ff439f04c219bf548b358fe6a
root <root@LAPTOP-EORMK77U.localdomain>
2021-12-10 16:28:34 +0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          Edit-2 <code>src/App.js</code> and save to reload.
+	  Edit-2 <code>src/App.js</code> and save to reload.
+		Edit 3
+        </p>
+	  <h2>Welcome to Docker</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
